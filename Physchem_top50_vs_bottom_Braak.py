53fd6ebcb2d2ffd8574d2827e8c815974d75884e
thudaUSF <65422059+thudaUSF@users.noreply.github.com>
2021-12-24 11:45:31 -0500

    Update Physchem_top50_vs_bottom_Braak.py

--- Physchem_top50_vs_bottom_Braak.py
+++ Physchem_top50_vs_bottom_Braak.py
@@ -1,6 +1,7 @@
 import traceback
 import pandas as pd
 import numpy as np
+import csv
 import os
 import sys
 #from lifelines.statistics import logrank_test
@@ -9,86 +10,90 @@ import sys
 #kmf = KaplanMeierFitter()
 #from lifelines import CoxPHFitter
 import scipy.stats as stats
+import rpy2.robjects as R
+from rpy2.robjects.packages import importr
+from rpy2.robjects import pandas2ri
+from rpy2.robjects.conversion import localconverter
 #import matplotlib.pyplot as plt
 #get_ipython().run_line_magic('matplotlib', 'inline')
 
+path = ""
 ##### recoveries data ##### 
-path_recoveries = "/Users/thuda/Desktop/ADphyschem/AD_pivot_pchem_with_clinical_data.csv"
+path_recoveries = path + "AD_pivot_pchem_with_clinical_data.csv"
 df = pd.read_csv(path_recoveries)
 
 ###### clinical data ####### (NOTE: FOR BOTTOM50/TOP50, WE CAN COMPARE BRAAK WITHIN AD ONLY, THEN COMPARE AD VS CONTROL)
-survival = "/Users/thuda/Desktop/ADphyschem/AD_pivot_pchem_with_clinical_data.csv"
+survival = path + "AD_pivot_pchem_with_clinical_data.csv"
 data_surv = pd.read_csv(survival, sep=",")
 data_clinical = data_surv[["SUBJID","BODY_SITE",
                            'APOE',
                            "Race",'AD']] 
+data_clinical = data_clinical.drop_duplicates(subset="SUBJID") 
 
 ###### SURVIVAL DATA #######
-path_survival = "/Users/thuda/Desktop/ADphyschem/AD_pivot_pchem_with_clinical_data.csv"
+path_survival = path + "AD_pivot_pchem_with_clinical_data.csv"
 data_surv = pd.read_csv(path_survival, sep=",")
-data_survival = data_surv[["SUBJID","Braak"]] #REMOVED AGE_BASELINE BECAUSE CAUSES AROUND 2000 BLANKS, then not included in processing
+data_surv = data_surv.drop_duplicates(subset="SUBJID") 
+#data_survival = data_surv[["SUBJID","Braak","BODY_SITE"]] #REMOVED AGE_BASELINE BECAUSE CAUSES AROUND 2000 BLANKS, then not included in processing
 
 ##### physchem data ##### 
-path_pchem = "/Users/thuda/Desktop/ADphyschem/AD_pchem.csv"
+path_pchem = path + "AD_pchem.csv"
 df_pchem = pd.read_csv(path_pchem)
-
 AD_patients = data_clinical.loc[data_clinical["AD"] == 1]["SUBJID"].drop_duplicates().tolist() #NOTE: TO FILTER ALL PATIENTS WITH VALUE OF 1;CHECK IF WORKING
 
-def sort_and_get_hi_lo(table,sampletype,receptor,proper):
-    try:
-        patients = table.loc[(sampletype,receptor), proper].sort_values(ascending=True)
-        length = len(patients)
-        middle_index = length//2
-        bottom_half = patients[:middle_index].index.tolist()
-        top_half = patients[middle_index:].index.tolist()
-    except:
-        bottom_half = []
-        top_half = []
-    
-    return bottom_half, top_half
+def runKW(pdf, proper):
+    pdf = pdf.rename(columns={proper: 'characteristic'})
+    groupsizes = pdf.groupby(["Braak"]).size().reset_index(name='Number of people')
+    finallist = groupsizes["Number of people"].tolist()
+    for i,braakn in groupsizes.iterrows():
+        if braakn[1] < 15:
+            pdf = pdf[pdf.Braak != braakn[0]]
+    with localconverter(R.default_converter + pandas2ri.converter):
+        df = R.conversion.py2rpy(pdf)
+    if proper=="fraction_negative":
+        pdf.to_csv(path + "something.csv")
+    Kruskall = R.r(r'''
+        function(df) {
+            options(warn=-1)
+            df$Braak <- as.factor(df$Braak)
+            levels(df$Braak)
+            df$Braak <- ordered(df$Braak, levels = c("0","3","5","6"))
 
-#NOTE: REPLACE WITH T-TEST
-def get_logrank(df1,df2):
-    results = stats.ttest_ind(df1['Braak'], df2['Braak'])
-    return results.pvalue
+            # run kruskal-wallis for property
+            pvalue <- kruskal.test(characteristic~Braak, data=df)$p.value
+            }
+    ''')
+    posthoc = R.r(r'''
+        function(df) {
+            options(warn=-1)
+            df$Braak <- as.factor(df$Braak)
+            levels(df$Braak)
+            df$Braak <- ordered(df$Braak, levels = c("0","3","5","6"))
 
-
-###### COMPARE BETTER SURVIVOR FN #### 
-def get_better_survivor(mean_braak_group1,mean_braak_group2):
-    if mean_braak_group1 > mean_braak_group2:
-        better_survivor = "Top50%"
-    elif mean_braak_group1 < mean_braak_group2:
-        better_survivor = "Bottom50%"
-    elif mean_braak_group1 == mean_braak_group2:
-        better_survivor = "Equal Survival"
-    else:
-        better_survivor = ""
-    return better_survivor
-
+            library(FSA)
+            dunn <- dunnTest(characteristic ~ Braak,
+                    data=df,
+                    method="bonferroni")$res
+            return(dunn)
+            }
+    ''')
+    rresults = Kruskall(df)
+    results=np.asarray(rresults)
+    if(results<1):
+        rposthoc = posthoc(df)
+    with localconverter(R.default_converter + pandas2ri.converter):
+        posthoc = R.conversion.rpy2py(rposthoc)
+    posthoclist = posthoc["P.adj"]
+    finallist.extend(results)
+    finallist.extend(posthoclist)
+    return finallist
 
 ###### GENERATE RESULTS OF ANALYSIS IN LIST FORMAT ##########    
-def get_data(sampletype,receptor,proper,df1,df2):
-    
-    N1 = len(df1["SUBJID"].to_list())
-    N2 = len(df2["SUBJID"].to_list())
-    
-    if N1 and N2 >= 2:
-        
-        Median_braak_group1 = df1["Braak"].median()
-        Median_braak_group2 = df2["Braak"].median()
-        Mean_braak_group1 = df1["Braak"].mean()
-        Mean_braak_group2 = df2["Braak"].mean()
-        pvalue = get_logrank(df1,df2)
-        
-        higher_survival = get_better_survivor(Mean_braak_group1,Mean_braak_group2)
-        
-        data = [sampletype,receptor,proper,
-                N1, N2, Median_braak_group1, Median_braak_group2,
-                Mean_braak_group1,Mean_braak_group2,pvalue,higher_survival]
-        
-    else:
-        data = [sampletype,receptor,proper,N1, N2,"", "","",""]
-        
+def get_data(sampletype,receptor,proper,df): 
+
+    list = runKW(df,proper)         
+    data = [sampletype,receptor,proper]
+    data.extend(list)
     return data
 
 # get patients ids for Bcell ALL
@@ -97,45 +102,52 @@ receptors = ["TRA","TRB","TRD","TRG","IGH","IGK","IGL"]
 properties = df_pchem.columns[3:] ###NOTE: DOES THIS DO ALL COLUMNS?
 #filetype = "wxs"
 
-results = []
+bloodresults = []
+brainresults = []
 
 for sampletype in sampletypes:
     for receptor in receptors:
 
         #### filter physchem df based on whether they have AD or not #### 
-        df_filtered_1 = df_pchem[df_pchem["SUBJID"].isin(AD_patients)]
+        df_filtered_1 = data_surv[data_surv["SUBJID"].isin(AD_patients)]
         
         #### filter reads based on sampletype and receptor type #### 
         df_filtered_2 = df_filtered_1[(df_filtered_1["BODY_SITE"] == sampletype) & (df_filtered_1["Receptor"] == receptor)]
-
+        if (df_filtered_2['SUBJID'].count() < 50):
+            continue
         #### generate corresponding pivot #####
-        ind = ["BODY_SITE","Receptor","SUBJID"]
-        table = pd.pivot_table(df_filtered_2, index = ind, values = df_filtered_2.columns, aggfunc = np.mean)
+        #ind = ["BODY_SITE","Receptor","SUBJID"]
+        #table = pd.pivot_table(df_filtered_2, index = ind, values = df_filtered_2.columns, aggfunc = np.mean)
         for proper in properties:
-            bottom_half, top_half = sort_and_get_hi_lo(table,sampletype,receptor,proper)
-            #print(bottom_half[:5])
-            df_survival_top50 = data_survival[data_survival.SUBJID.isin(top_half)].dropna().drop_duplicates()
-            df_survival_bottom50 = data_survival[data_survival.SUBJID.isin(bottom_half)].dropna().drop_duplicates()
-            if proper == "fraction_negative" and sampletype == "Brain" and receptor== "TRA":
-                #patients = patients.merge(df_pchem[['SUBJID','fraction_negative']],on='SUBJID', how='left') #cc = control, case
-                df_survival_top50.to_csv("/Users/thuda/Desktop/ADphyschem/trabrainfraction_negativepchemt50.csv",index=False)
-                df_survival_bottom50.to_csv("/Users/thuda/Desktop/ADphyschem/trabrainfraction_negativepchemb50.csv",index=False)
-            
-            
-            
-            
-            data = get_data(sampletype,receptor,proper,df_survival_top50,df_survival_bottom50)
-            results.append(data)
+            table = df_filtered_2[["SUBJID","Braak",proper]] 
+            table.to_csv(path + "someting.csv")
+            table.loc[(table['Braak'] == 4), 'Braak'] = 3
+            if sampletype=='Blood':
+                table.loc[(table['Braak'] < 3), 'Braak'] = 0                
+           
+            data = get_data(sampletype,receptor,proper,table)
+            if sampletype=='Blood':
+                bloodresults.append(data)
+            if sampletype=='Brain':
+                brainresults.append(data)
 
+Detailsblood = ["Sampletype","Receptor","Property",
+                "N - Braak 0, I, II","N - Braak III, IV",
+                "N - Braak V", "N - Braak VI", "KW p-value",
+                "Post-hoc Comparison 0,I,II/III,IV", "Post-hoc Comparison 0,I,II/V",
+                "Post-hoc Comparison 0,I,II/VI","Post-hoc Comparison III,IV/V",
+                "Post-hoc Comparison III,IV/VI","Post-hoc Comparison V/VI"]
+Detailsbrain = ["Sampletype","Receptor","Property",
+                "N - Braak 0, I, II","N - Braak III, IV",
+                "N - Braak V", "N - Braak VI", "KW p-value",
+                "Post-hoc Comparison III,IV/V",
+                "Post-hoc Comparison III,IV/VI","Post-hoc Comparison V/VI"]
 
-
-cols_results = ["Sampletype","Receptor","Property",
-                "N - top50%","N - bottom50%",
-                "Median braak - top50%", "Median braak - bottom50%",
-                "Mean braak - top50%", "Mean braak - bottom50%",
-                "P-value","Worse Braak"]
-df_results = pd.DataFrame(results)
-df_results.columns = cols_results 
-df_results = df_results.dropna().sort_values(by=["P-value"]) #NOT WORKING
-results_path = "/Users/thuda/Desktop/ADphyschem/results.xlsx" #GET DATE AND TIME AS STRING
-df_results.to_excel(results_path, index = False)
+df_bloodresults = pd.DataFrame(bloodresults)
+df_brainresults = pd.DataFrame(brainresults)
+df_bloodresults.columns = Detailsblood 
+df_brainresults.columns = Detailsbrain
+df_bloodresults = df_bloodresults.dropna().sort_values(by=["KW p-value"])
+df_brainresults = df_brainresults.dropna().sort_values(by=["KW p-value"])
+df_bloodresults.to_csv(path + "bloodresults.csv", index = False)
+df_brainresults.to_csv(path + "brainresults.csv", index = False)
